d4e763cd1f60ee1ea3ced42c2d375a20e4c7838e
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-04-08 02:33:43 -0400

    trying to make text boxes

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 $.getJSON("data.json", function(data) {
-    for (var questions in JSON) {
+    for (var questions in data.questions) {
+        let result; 
         if (sum < 5) { // did not answer all questions
-
+            document.getElementById("toggle_answer").innerHTML = "Please complete all questions!";
+            
         } 
-
         else if (sum == 5) { // Lazy
+            result = "Lazy Varsha"
             
         }
         else if (sum <= 10) { // Healthy
-
+            result = "Healthy Varsha"
         }
         else if (sum <= 15) { // Hardo
-
+            result = "Hardo Varsha"
         }
-        else { // Sadboi
+        else { // Sadboi 15+ 
+            result = "Sadboi Varsha"
+        }
 
+        $("#result p").text("You are " + result);
+        $("#result").css("display", "flex");
+
+        let image;
+        switch(result){
+            case ('Lazy Varsha'):
+                image = 'lazy';
+                break;
+            case ('Healthy Varsha'):
+                image = 'healthy';
+                break;
+            case('Hardo Varsha'):
+                image = 'hardo';
+                break;
+            default: // Sadboi Varsha
+                image = 'hardo'; 
+                break;
         }
     }
     // now you can do something with this data. 
