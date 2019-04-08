53475abe96e935b11ba869018db9e24c9f51347f
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-04-07 23:40:27 -0400

    basic layout of quiz

---
+++ main.js
@@ -0,0 +1,24 @@
+$.getJSON("data.json", function(data) {
+    for (var questions in JSON) {
+        if (sum < 5) { // did not answer all questions
+
+        } 
+
+        else if (sum == 5) { // Lazy
+            
+        }
+        else if (sum <= 10) { // Healthy
+
+        }
+        else if (sum <= 15) { // Hardo
+
+        }
+        else { // Sadboi
+
+        }
+    }
+    // now you can do something with this data. 
+    // remember you can only work with the data in this callback
+    // data.title has the title
+    // maybe you want to loop through data.questions? 
+  });
