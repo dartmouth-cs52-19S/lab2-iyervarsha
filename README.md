c9715b5664e3879ef6636f6e683600711ff5a7b8
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-04-09 23:22:09 -0400

    final submission

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # BuzzFeed Quiz
 # Varsha Iyer
-# 9/7/19
+# 4/9/19
 
 ## Why might it be better for the main.js link to be placed at the bottom rather than in <head> ... </head>
 
 Linking this at the bottom allows the web page and HTML content to load before executing JavaScript. It doesn't make sense for the "calculating part" to happen until the page loads. 
 
 ## Description of what you did
-I wrote a buzzfeed quiz based on what type of Varsha someone is: lazy varsha, healthy varsha, hardo varsha, and sadboi varsha. 
+I wrote a buzzfeed quiz based on what type of Varsha someone is: lazy varsha, healthy varsha, hardo varsha, and sadboi varsha. I chose the 2nd option for the choose your own adventure. I calculated my results by using numerical values and selecting the "type of varsha" according to the sum of the checkbox values.
 ### What worked 
 #### Made Some Components on Figma!
+I made my landing page on figma. I also made some of my pictures on Figma as well (such as the ones in the first question).
 #### Transformations/ Animations
+My buttons animate when you hover and click on them. The hover animation is a fade while the clicking animation is a border. I made the textboxes animate with java script and I made the images animate with CSS. 
 #### Smooth Scrolling for Landing Page
+When you click start, the page scrolls down. 
 #### Flashing Text
+On my landing page, I got my header to flash different colors. 
 #### Modals
+Made my modals slide down. 
 
 
 ### What didn’t work.
 #### JSON
 I tried for a while to get the JSON file to work, but didn't have enough time to finish this portion. 
+
--- main.js
+++ main.js
@@ -1,176 +1,150 @@
-$(document).ready(function(){
+function addBorder() {
+    document.getElementById("container").style.transition("all .2s linear");
+    document.getElementById("container").style.addBorder("10px solid #285FEC");
+}
 
-    function addBorder() {
-        document.getElementById("container").style.transition("all .2s linear");
-        document.getElementById("container").style.addBorder("10px solid #285FEC");
+    // Calculating Results: 
+
+    $('#submit').on('click', function(event) {
+    // gather all checked radio-button values
+    event.preventDefault(); // prevents screen from reloading
+    console.log("submitted");
+
+    $("#result").addClass("result-show");
+    $("#result").removeClass("result-hide");
+    $("#warning").css("display", "none");
+    $("#result").css("display", "none");
+
+    var choices = $("input[type='radio']:checked").map(function(i, radio) {
+        return $(radio).val();
+    }).toArray();
+
+
+    var total = 0;
+    let result;
+    let description;
+
+    // If the quiz is filled out
+    if (choices.length < 5) {
+        result = "WARNING";
+        description = "Are You Sure You Didn't Miss a Question?"
+        document.getElementById('modal-text').innerHTML += result;
+        document.getElementById('modal-description').innerHTML += description;
+
+        // https://www.w3schools.com/howto/howto_css_modals.asp
+
+        // Get the modal
+        var modal = document.getElementById('results');
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("submit");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on the button, open the modal 
+            modal.style.display = "flex";
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            document.getElementById('modal-text').innerHTML = "";
+            document.getElementById('modal-description').innerHTML = "";
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                document.getElementById('modal-text').innerHTML = "";
+                document.getElementById('modal-description').innerHTML = "";
+                modal.style.display = "none";
+            }
+        }
+        
     }
-    // Dynamically Loading Data 
-    $.getJSON("data.json", function(data) {
-        console.log(data);
-        for(var question in data.questions) {
-            console.log(question);
-            var $newdiv1 = document.createElement( "div" ) // creating a div to append to 
-            // $("body").append($newdiv1);
-            // $($newdiv1).html("varsha");
+    if (choices.length == 5) {
+        for (var i = 0; i < choices.length; i++){
+            total += JSON.parse(choices[i]);
         }
-        // now you can do something with this data. 
-        // remember you can only work with the data in this callback
-        // data.title has the title
-        // maybe you want to loop through data.questions? 
-      });
-    
-      // Calculating Results: 
 
-      $('#submit').on('click', function(event) {
-        // gather all checked radio-button values
-        event.preventDefault(); // prevents screen from reloading
-        console.log("submitted");
+        
+        if (total <= 8) {
+            console.log(total);
+            console.log("You Got: Lazy Varsha");
+            result = "Lazy Varsha";
+            description = "You probs haven't gone to the gym in 3 terms...Idk what to tell you man...";
+            $("#healthy").removeClass("modal-image-show");
+            $("#sadboi").removeClass("modal-image-show");
+            $("#hardo").removeClass("modal-image-show");
+            $("#lazy").addClass("modal-image-show");
 
-        $("#result").addClass("result-show");
-        $("#result").removeClass("result-hide");
-        $("#warning").css("display", "none");
-        $("#result").css("display", "none");
+        }
 
-        var choices = $("input[type='radio']:checked").map(function(i, radio) {
-          return $(radio).val();
-        }).toArray();
+        if (total > 8 && total <= 12 ) {
+            console.log(total);
+            console.log("You Got: Healthy Varsha");
+            result = "Healthy Varsha";
+            description = "Ok, I'm sorry, but you're really the odd one out here";
+            $("#healthy").addClass("modal-image-show");
+            $("#sadboi").removeClass("modal-image-show");
+            $("#hardo").removeClass("modal-image-show");
+            $("#lazy").removeClass("modal-image-show");
 
+        }
 
-        var total = 0;
-        let result;
-        let description;
+        if(total > 12 && total <= 16) {
+            console.log(total);
+            console.log("You Got: Hardo Varsha");
+            result = "Hardo Varsha";
+            description = "I hope your GPA went up at least...";
+            $("#hardo").addClass("modal-image-show");
+            $("#healthy").removeClass("modal-image-show");
+            $("#sadboi").removeClass("modal-image-show");
+            $("#lazy").removeClass("modal-image-show");
 
-        // If the quiz is filled out
-        if (choices.length < 5) {
-            result = "WARNING";
-            description = "Are You Sure You Didn't Miss a Question?"
-            document.getElementById('modal-text').innerHTML += result;
-            document.getElementById('modal-description').innerHTML += description;
+        }
 
-            // https://www.w3schools.com/howto/howto_css_modals.asp
+        if (total > 16 && total <=20){
+            console.log(total);
+            console.log("You Got: Sadboi Varsha");
+            result = "Sadboi Varsha";
+            description = "Honestly, why are you taking this quiz? Please sleep...";
+            $("#sadboi").addClass("modal-image-show");
+            $("#healthy").removeClass("modal-image-show");
+            $("#lazy").removeClass("modal-image-show");
+            $("#hardo").removeClass("modal-image-show");
 
-            // Get the modal
-            var modal = document.getElementById('results');
+        }
 
-            // Get the button that opens the modal
-            var btn = document.getElementById("submit");
+        document.getElementById('modal-text').innerHTML += result;
+        document.getElementById('modal-description').innerHTML += description;
 
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
+        // https://www.w3schools.com/howto/howto_css_modals.asp
 
-            // When the user clicks on the button, open the modal 
-                modal.style.display = "flex";
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
+        // Get the modal
+        var modal = document.getElementById('results');
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("submit");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on the button, open the modal 
+            modal.style.display = "flex";
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            document.getElementById('modal-text').innerHTML = "";
+            document.getElementById('modal-description').innerHTML = "";
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
                 document.getElementById('modal-text').innerHTML = "";
                 document.getElementById('modal-description').innerHTML = "";
                 modal.style.display = "none";
             }
-
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-                if (event.target == modal) {
-                    document.getElementById('modal-text').innerHTML = "";
-                    document.getElementById('modal-description').innerHTML = "";
-                    modal.style.display = "none";
-                }
-            }
-            
         }
-        if (choices.length == 5) {
-            for (var i = 0; i < choices.length; i++){
-                total += JSON.parse(choices[i]);
-            }
-
-            
-            if (total <= 8) {
-                console.log(total);
-                console.log("You Got: Lazy Varsha");
-                result = "Lazy Varsha";
-                description = "You probs haven't gone to the gym in 3 terms...Idk what to tell you man...";
-                $("#healthy").removeClass("modal-image-show");
-                $("#sadboi").removeClass("modal-image-show");
-                $("#hardo").removeClass("modal-image-show");
-                $("#lazy").addClass("modal-image-show");
-    
-            }
-    
-            if (total > 8 && total <= 12 ) {
-                console.log(total);
-                console.log("You Got: Healthy Varsha");
-                result = "Healthy Varsha";
-                description = "Ok, I'm sorry, but you're really the odd one out here";
-                $("#healthy").addClass("modal-image-show");
-                $("#sadboi").removeClass("modal-image-show");
-                $("#hardo").removeClass("modal-image-show");
-                $("#lazy").removeClass("modal-image-show");
-    
-            }
-    
-            if(total > 12 && total <= 16) {
-                console.log(total);
-                console.log("You Got: Hardo Varsha");
-                result = "Hardo Varsha";
-                description = "I hope your GPA went up at least...";
-                $("#hardo").addClass("modal-image-show");
-                $("#healthy").removeClass("modal-image-show");
-                $("#sadboi").removeClass("modal-image-show");
-                $("#lazy").removeClass("modal-image-show");
-    
-            }
-    
-            if (total > 16 && total <=20){
-                console.log(total);
-                console.log("You Got: Sadboi Varsha");
-                result = "Sadboi Varsha";
-                description = "Honestly, why are you taking this quiz? Please sleep...";
-                $("#sadboi").addClass("modal-image-show");
-                $("#healthy").removeClass("modal-image-show");
-                $("#lazy").removeClass("modal-image-show");
-                $("#hardo").removeClass("modal-image-show");
-    
-            }
-
-            document.getElementById('modal-text').innerHTML += result;
-            document.getElementById('modal-description').innerHTML += description;
-
-            // https://www.w3schools.com/howto/howto_css_modals.asp
-
-            // Get the modal
-            var modal = document.getElementById('results');
-
-            // Get the button that opens the modal
-            var btn = document.getElementById("submit");
-
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-
-            // When the user clicks on the button, open the modal 
-                modal.style.display = "flex";
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-                document.getElementById('modal-text').innerHTML = "";
-                document.getElementById('modal-description').innerHTML = "";
-                modal.style.display = "none";
-            }
-
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-                if (event.target == modal) {
-                    document.getElementById('modal-text').innerHTML = "";
-                    document.getElementById('modal-description').innerHTML = "";
-                    modal.style.display = "none";
-                }
-            }
-        }
-        // else {
-        //     result = "WARNING";
-        //     description = "Did You Answer All of the Questions?";
-        //     document.getElementById('modal-text').innerHTML += result;
-        //     $("#warning").css("display", "flex");
-        // }
-
-
-      });
-    
-});
+    }
+});  
